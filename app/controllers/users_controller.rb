6a8cc4018aa2401ac899a8227cc307c3c71fcfda
joshua richards <jermain119@gmail.com>
2019-12-02 22:49:15 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,30 @@ end
     else 
       # binding.pry 
       redirect to '/signup'
-   end
+    end
  end
 
-end
+    get '/login' do 
+      if logged_in?
+        erb :'users/login'
+        
+      else 
+        
+      redirect to '/tweets'
+   end
+end 
+
+post 'login' do
+  @user = User.find_by(:username => params)
+[:username])
+    if @user && @user.authenticate(params[  :password])
+    session[:user_id] = @user.id 
+    redirect to "/tweets"
+    else 
+  redirect to "/login"
+  
+    end
+  
+  end 
+
+end 
