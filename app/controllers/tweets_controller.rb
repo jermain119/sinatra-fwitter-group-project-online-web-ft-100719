9ee2294d53742919956af5a147289167c5be38f9
joshua richards <jermain119@gmail.com>
2019-12-02 23:17:15 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,12 @@
 class TweetsController < ApplicationController
 
-get '/tweets' do #index
-  @tweets =Tweet.all 
- 
-  end
-end
+ get '/tweets' do 
+      if logged_in? 
+        @tweets = Tweet.all
+        erb :'/tweets/index'
+      else 
+        redirect to '/login'
+      end 
+    end 
+    
+  end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,28 +21,32 @@ end
       redirect to '/signup'
     end
  end
-
-    get '/login' do 
-      if logged_in?
-        erb :'users/login'
-        
-      else 
-        
-      redirect to '/tweets'
-   end
-end 
-
-post 'login' do
-  @user = User.find_by(:username => params)
-[:username])
-    if @user && @user.authenticate(params[  :password])
-    session[:user_id] = @user.id 
-    redirect to "/tweets"
+ 
+ get '/login' do
+    if !logged_in? 
+      erb :'users/login'
     else 
-  redirect to "/login"
-  
-    end
-  
+      redirect '/tweets'
+    end  
   end 
 
+  post '/login' do 
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect '/tweets'
+    else 
+      redirect '/login'
+    end 
+  end 
+
+  get '/logout' do 
+    if logged_in? 
+      session.destroy 
+      redirect '/login'
+    else 
+      redirect to '/'
+    end 
+  end
+   
 end 
