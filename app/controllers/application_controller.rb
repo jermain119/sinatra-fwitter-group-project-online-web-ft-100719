89fce58b00929f79aed4b209d894af096ef59cfd
joshua richards <jermain119@gmail.com>
2019-12-02 02:20:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/signup' do
-      
+      User.create(username: params[:name], email: params[:email], password: params[:password])
       redirect to '/signup'
    end
    
