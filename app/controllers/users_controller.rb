937d897640ee2ad9cb30c0d9febda87418347e29
joshua richards <jermain119@gmail.com>
2019-12-05 22:45:33 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params["username"] != "" && params["email"] != "" && params["password"] != ""
+    if params[:username] != "" && params[:email] != "" && params["password"] != ""
     @user = User.create(username: params['username'], email: params['email'], password: params['password'])
     session[:user_id] = @user.id
     redirect to '/tweets'
